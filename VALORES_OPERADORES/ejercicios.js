3bf7482c5c9314710ca3d34c14cf3c2854231db8
yerson34 <132001298+yerson34@users.noreply.github.com>
2024-09-25 13:19:46 -0500

    ejercicios si estaran bien

---
+++ VALORES_OPERADORES/ejercicios.js
@@ -0,0 +1,42 @@
+//convertir expresiones aritmeticas 
+// expresion algoritmos
+// expresiones linial
+console.log()
+
+//EJERCICIO 01
+
+console.log(12 / 21 + 21 / 33) / ((12 / 21) - (21 / 3))
+
+//EJERCICIO 02
+
+console.log(((12 + (21 / (12 + 21 + (21 / 33)))) / (12 + (21 / (33 + 12)))))
+//EJERCICIO 03
+
+console.log(((12 + 21 + 33) / (12 + (21 / 33))))
+
+//EJERCICIO 04
+
+console-log(((12 + 21 + (33 / (21 * 12))) / (12 + 21 * (33 / 44))))
+
+//EJERCICIO 05
+
+console.log((12 + (21 / 33) + 44) / 12)
+
+//EJERCICIO 06
+
+console.log(12 + ((12 + (12 + 21) / (33 + 44)) / ((12 + (12 / 21)))));
+
+//EJERCICIO 7
+
+console.log ( 12 + 21 + (33 / 44) + ((12 / ((21 - 33) / 12)) / (21 + 33)))
+
+
+
+
+
+
+
+
+
+
+
