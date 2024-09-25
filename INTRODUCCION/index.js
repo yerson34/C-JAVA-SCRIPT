03912d7f96ab85c1efef9737293ab3cb1b65d331
yerson34 <132001298+yerson34@users.noreply.github.com>
2024-09-25 13:21:09 -0500

    si estara bien

--- INTRODUCCION/index.js
+++ INTRODUCCION/index.js
@@ -1,1 +1,13 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Capitulo 2</h1>
+    <script src="./operadores.JS"></script>
+    <script src="./ejercicios.js"></script>
+</body>
+</html>
