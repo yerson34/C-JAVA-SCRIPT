c6cb8d2af47988a9cab3db81373ee40d81a8ee1f
yerson34 <132001298+yerson34@users.noreply.github.com>
2024-10-02 13:18:48 -0500

    Add files via upload
    ternarios ejerciccios

---
+++ VALORES_OPERADORES/ternarios.js
@@ -0,0 +1,9 @@
+// ejercicios 
+// usando el operador ternarioevaluar si un numero es par o impar
+numero = 10;
+resultado = (numero % 2 == 0) ? "Par" : "Impar";
+System.out.println("El número es: " + resultado);
+//mostrar un mensaje si la edad de unap ersona es mayor a 19
+ edad = 20;
+mensaje = (edad > 19) ? "Mayor a 19" : "Menor o igual a 19";
+System.out.println(mensaje);
