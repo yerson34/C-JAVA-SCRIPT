29646c0d6f56fd397ff5182e5f7531fda81cf1b4
yerson34 <132001298+yerson34@users.noreply.github.com>
2024-10-09 13:03:17 -0500

    029

---
+++ ESCTRUCTURA DEL PROGRAMA/alerta.js
@@ -0,0 +1,13 @@
+// para mostrar un mensaje en mi ventana
+alert("hola")
+
+//para pedir al usuario que elija una opcion
+respuestaConfirm=confirm("escoje")
+
+//para pedir al usuario que ingres una informacion
+respuestaPrompt=prompt("escribe algo mascota")
+
+console.log(`
+  respuesta de confirm: ${respuestaConfirm},
+  respuesta de prompt: ${respuestaPrompt}
+  `)
